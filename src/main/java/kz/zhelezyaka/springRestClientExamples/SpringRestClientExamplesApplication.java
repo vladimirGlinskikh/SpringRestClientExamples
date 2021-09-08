b21f4da3e78ed566e386c06a863675175ef61fde
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-09-08 22:17:33 +0600

    Created domain entity

---
+++ src/main/java/kz/zhelezyaka/springRestClientExamples/SpringRestClientExamplesApplication.java
@@ -0,0 +1,11 @@
+package kz.zhelezyaka.springRestClientExamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestClientExamplesApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestClientExamplesApplication.class, args);
+    }
+}
