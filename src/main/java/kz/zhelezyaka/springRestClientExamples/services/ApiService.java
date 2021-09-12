a99b6e1d2f128284603c1852fdab9abbfa9f2232
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-09-12 11:58:13 +0600

    Going Reactive with Spring WebClient

--- src/main/java/kz/zhelezyaka/springRestClientExamples/services/ApiService.java
+++ src/main/java/kz/zhelezyaka/springRestClientExamples/services/ApiService.java
@@ -1,9 +1,13 @@
 package kz.zhelezyaka.springRestClientExamples.services;
 
 import kz.zhelezyaka.api.domain.User;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
 public interface ApiService {
     List<User> getUsers(Integer limit);
+
+    Flux<User> getUsers(Mono<Integer> limit);
 }
--- src/main/java/kz/zhelezyaka/springRestClientExamples/services/ApiServiceImpl.java
+++ src/main/java/kz/zhelezyaka/springRestClientExamples/services/ApiServiceImpl.java
@@ -2,9 +2,13 @@ package kz.zhelezyaka.springRestClientExamples.services;
 
 import kz.zhelezyaka.api.domain.User;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.UriComponentsBuilder;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
@@ -27,5 +31,17 @@ public class ApiServiceImpl implements ApiService {
         List<User> userData = restTemplate.getForObject(uriComponentsBuilder.toUriString(), List.class);
 
         return userData;
+    }
+
+    @Override
+    public Flux<User> getUsers(Mono<Integer> limit) {
+        return WebClient
+                .create(api_url)
+                .get()
+                .uri(uriBuilder -> uriBuilder.queryParam("_limit", limit.toProcessor().block()).build())
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .flatMap(resp -> resp.bodyToMono(List.class))
+                .flatMapIterable(list -> list);
     }
 }
